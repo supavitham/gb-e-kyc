c8711518d76eee78c5b1e80ebd4ba08a3b293518
supavitham <supavitham@gmail.com>
2022-03-23 13:37:58 +0700

    .

---
+++ lib/e_kyc_button_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:gb_e_kyc/e_kyc_screen.dart';
+
+class EKYCButtonWidget extends StatefulWidget {
+  const EKYCButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  State<EKYCButtonWidget> createState() => _EKYCButtonWidgetState();
+}
+
+class _EKYCButtonWidgetState extends State<EKYCButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const EKYCScreen())),
+      child: Container(
+        // width: 60,
+        // height: 80,
+        padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: const Center(
+          child: Text(
+            "KYC",
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.black
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/e_kyc_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class EKYCScreen extends StatefulWidget {
+  const EKYCScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EKYCScreen> createState() => _EKYCScreenState();
+}
+
+class _EKYCScreenState extends State<EKYCScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("KYC Screen"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Text("test -----"),
+        ],
+      ),
+    );
+  }
+}
